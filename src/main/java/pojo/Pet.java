02040af6b1eed1074ac5e921895473d61873cf51
suat525255 <suataydin5255@gmail.com>
2025-01-04 20:16:12 +0300

    added pojo

---
+++ src/main/java/pojo/Pet.java
@@ -0,0 +1,65 @@
+package pojo;
+
+import java.util.List;
+
+public class Pet {
+    private int id;
+    private String name;
+    private String status;
+    private Category category;
+    private List<String> photoUrls;
+    private List<Tag> tags;
+
+
+    public Pet() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+}
